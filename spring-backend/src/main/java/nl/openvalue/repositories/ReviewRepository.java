6a22d160a7c3378f02121a5307c7dbeb0c54b7a2
Rachnerd <rachel_149@hotmail.com>
2019-08-15 19:15:42 +0200

    WIP

--- spring-backend/src/main/java/nl/openvalue/repositories/ReviewRepository.java
+++ spring-backend/src/main/java/nl/openvalue/repositories/ReviewRepository.java
@@ -2,7 +2,11 @@ package nl.openvalue.repositories;
 
 import nl.openvalue.entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-
+    @Query("SELECT R FROM Review R WHERE R.userId=:id")
+    public List<Review> findByUserId(Long id);
 }
